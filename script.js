696e96042b719cfc539d3e984b556f65fa3377e5
Imagine <91915381+ImaginePog@users.noreply.github.com>
2023-03-26 05:58:30 +0545

    Add computer choice function

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function GetUserChoice() {
 
     return userChoice;
 }
+
+const options = ["rock","paper","scissors"];
+
+function GetComputerChoice() {
+    let computerChoice;
+    let randNum = Math.floor(Math.random() * 3);
+    computerChoice = options[randNum];
+
+    return computerChoice;
+}
+
