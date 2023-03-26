8882e5fd6cc71fdfaa8b8e699cb4cdba4003ba5b
Imagine <91915381+ImaginePog@users.noreply.github.com>
2023-03-26 06:09:47 +0545

    Log the choices in the console

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function GetUserChoice() {
-    let userChoice = prompt("Enter your choice: (rock,paper,scissor): ");
+    let userChoice = prompt("Enter your choice: (rock,paper,scissors): ");
     userChoice = userChoice.toLowerCase();
-
+    
+    console.log(userChoice);
+    
     return userChoice;
 }
 
@@ -11,7 +13,8 @@ function GetComputerChoice() {
     let computerChoice;
     let randNum = Math.floor(Math.random() * 3);
     computerChoice = options[randNum];
+    
+    console.log(computerChoice);
 
     return computerChoice;
 }
-
